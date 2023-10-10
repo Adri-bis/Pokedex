b0ec114e04b0cd67d639769ba8ad6114d3bc5303
Adri-bis <adrien.ma.16@hotmail.fr>
2023-10-10 17:33:27 +0200

    Proptype

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,20 @@
-
+import PropType from "prop-types";
 
 function PokemonCard({ pokemon }) {
 
     console.log(pokemon);
+
     return <figure>{pokemon.imgSrc ? <img img src={pokemon.imgSrc} alt={pokemon.alt}></img> : <p>???</p>}
         <figcaption>{pokemon.name}</figcaption>
     </figure >
 }
 
+PokemonCard.PropType = {
+    pokemon: PropType.shape({
+        name: PropType.string.isRequired,
+        imgSrc: PropType.string.isRequired,
+        alt: PropType.string.isRequired,
+    })
+
+}
 export default PokemonCard;
